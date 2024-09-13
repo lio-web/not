11e2e5497b971ec5576bab672d7352a74fc83df0
lionard <leonard.muhati@student.moringaschool.com>
2024-09-13 14:46:59 +0300

    compelete

--- Code-challange/src/components/BotCollection.js
+++ Code-challange/src/components/BotCollection.js
@@ -1,28 +1,23 @@
 import React, { useEffect, useState } from "react";
+import BotCard from "./BotCard"; // Import BotCard component
 
-function BotCollection({onAddBot, OnDeleteBot}) {
-  // Your code here
-const [bots, setBots]=useState([])
+function BotCollection({ onAddBot, onDeleteBot }) {
+  const [bots, setBots] = useState([]);
 
-useEffect(()=>{
-  fetch("/bots")
-  .then(response=> response.json())
-  .then(data=>setBots(data))
-  .catch(error=>console.error("Error fetching boots:", error));
-},[]);
+  useEffect(() => {
+    fetch("/bots")
+      .then((response) => response.json())
+      .then((data) => setBots(data))
+      .catch((error) => console.error("Error fetching bots:", error));
+  }, []);
 
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        {bots.map(bot=>(
-           <div className="column" key={bot.id}>
-           <BotCard
-             bot={bot}
-             onAddBot={onAddBot}
-             onDeleteBot={onDeleteBot}
-           />
-           </div>
+        {bots.map((bot) => (
+          <div className="column" key={bot.id}>
+            <BotCard bot={bot} onAddBot={onAddBot} onDeleteBot={onDeleteBot} /> 
+          </div>
         ))}
         Collection of all bots
       </div>
